a38849c018eb171479fea1d8162b38e29822bc38
Tan Theng Lee <thengleetan@gmail.com>
2021-02-18 15:07:29 +0800

    Import cmp package from remote module

--- hello.go
+++ hello.go
@@ -1,10 +1,13 @@
 package main
 
 import (
-	"example.com/user/hello/morestrings"
 	"fmt"
+
+	"example.com/user/hello/morestrings"
+	"github.com/google/go-cmp/cmp"
 )
 
 func main() {
 	fmt.Println(morestrings.ReverseRunes("!oG, olleH"))
+	fmt.Println(cmp.Diff("Hello World", "Hello Go"))
 }
